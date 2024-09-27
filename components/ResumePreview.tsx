b2d6aa885977d86cfcaefb7fc548867abc67fc5f
Ankush Makkar <ankushmakkar108@gmail.com>
2024-09-27 22:34:48 +0530

    demo data for devs

--- components/ResumePreview.tsx
+++ components/ResumePreview.tsx
@@ -1,5 +1,9 @@
 import { Document, Page, Text, View, StyleSheet, Link, Font } from '@react-pdf/renderer';
 import { ResumeData } from '@/lib/types';
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+});
 
 const ResumePreview = ({ resumeData }: { resumeData: ResumeData }) => (
   <Document>
@@ -149,11 +153,6 @@ const ResumePreview = ({ resumeData }: { resumeData: ResumeData }) => (
     </Page>
   </Document>
 );
-
-Font.register({
-  family: 'Oswald',
-  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
-});
 
 const styles = StyleSheet.create({
   page: {
